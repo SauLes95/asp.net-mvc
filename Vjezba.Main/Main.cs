b664bd7276dfb8a47ae21aa379f5fa1f021cc0a2
SauLes95 <sestaklaura11@gmail.com>
2024-03-15 12:47:01 +0100

    Implemented async tasks with Task.WaitAll() for delays of 1s and 1.5s, with clear console output.

---
+++ Vjezba.Main/Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vjezba.Main
+{
+    internal class MainClass
+    {
+        
+        static Task t1 = Task.Run(() =>
+        {
+            Console.WriteLine($"Sleeping T1 started");
+            Thread.Sleep(1000);
+            Console.WriteLine($"Sleeping T1 completed");
+        });
+
+        static Task t2 = Task.Run(() =>
+        {
+            Console.WriteLine($"Sleeping T2 started");
+            Thread.Sleep(1500);
+            Console.WriteLine($"Sleeping T2 completed");
+        });
+
+        public static void Main(string[] args)
+        {
+            Task.WaitAll(t1, t2);
+            //Task.WaitAny(t1, t2);   
+        }
+        
+    }
+}
