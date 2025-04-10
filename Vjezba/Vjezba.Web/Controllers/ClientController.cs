3cfb925043384bc53c76206af742107f5cdebe38
SauLes95 <sestaklaura11@gmail.com>
2025-04-10 21:54:16 +0200

    Enable navigation to client details page from client list in Index view and add breadcrumb navigation in Details view with a return option

--- Vjezba/Vjezba.Web/Controllers/ClientController.cs
+++ Vjezba/Vjezba.Web/Controllers/ClientController.cs
@@ -16,6 +16,20 @@ namespace Vjezba.Web.Controllers
 		public IActionResult Details(int? id)
 		{
 			var model = id != null ? MockClientRepository.Instance.FindByID(id.Value) : null;
+			if ( id > MockCityRepository.Instance.All().ToList().Count)
+			{
+				return View(null);
+			}
+
+			List<City> cityList = MockCityRepository.Instance.All().ToList();
+
+			foreach (var city in cityList)
+			{
+				if (model.CityID == city.ID)
+				{
+					ViewBag.City = city.Name;
+				}
+			}
 			return View(model);
 		}
 	}
