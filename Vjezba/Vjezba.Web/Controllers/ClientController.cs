8d6213a3c930da8e6119031c6a4f519728576ba4
SauLes95 <sestaklaura11@gmail.com>
2025-04-10 12:36:24 +0200

    Created ClientController with Index action to display list of clients. Added Index.cshtml view to show the total number of clients using Bootstrap 'alert' component.

---
+++ Vjezba/Vjezba.Web/Controllers/ClientController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vjezba.Web.Mock;
+
+namespace Vjezba.Web.Controllers
+{
+	public class ClientController : Controller
+	{
+		public IActionResult Index()
+		{
+			List<Client> clientList = MockClientRepository.Instance.All().ToList();
+
+			return View(clientList);
+		}
+	}
+}
