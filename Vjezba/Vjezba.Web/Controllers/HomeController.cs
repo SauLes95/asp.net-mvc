19c0a307d913d9e8585939357194eccb0448d17a
SauLes95 <sestaklaura11@gmail.com>
2025-04-10 11:09:35 +0200

    Created and designed the contact form using Bootstrap grid system and custom styling.

--- Vjezba/Vjezba.Web/Controllers/HomeController.cs
+++ Vjezba/Vjezba.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using System.Collections.Immutable;
 using System.Diagnostics;
 using Vjezba.Web.Models;
 
@@ -21,10 +23,6 @@ namespace Vjezba.Web.Controllers
         public IActionResult Contact()
         {
             ViewBag.Message = "Jednostavan način proslijeđivanja poruke iz Controller -> View.";
-            //Kao rezultat se pogled /Views/Home/Contact.cshtml renderira u "pravi" HTML
-            //Primjetiti - View() je poziv funkcije koja uzima cshtml template i pretvara ga u HTML
-            //Zasto bas Contact.cshtml? Jer se akcija zove Contact, te prema konvenciji se "po defaultu" uzima cshtml datoteka u folderu Views/CONTROLLER_NAME/AKCIJA.cshtml
-
 
             return View();
         }
@@ -44,14 +42,22 @@ namespace Vjezba.Web.Controllers
 		[HttpPost]
         public IActionResult SubmitQuery(IFormCollection formData)
         {
-            //Ovdje je potrebno obraditi podatke i pospremiti finalni string u ViewBag
+            var fullName = formData["firstName"] + " " + formData["lastName"];
+			var email = formData["email"];
+			var message = formData["message"];
+            var messageType = formData["messageType"];
+			var newsletter = formData["newsletter"] == "on";
+			string newsletterMessage = newsletter ? "obavijestit ćemo vas" : "nećemo vas obavijestiti";
 
-            
+            TempData["Message"] = $"Poštovani {fullName} ({email}) zaprimili smo vašu poruku te će vam se netko ubrzo javiti. Sadržaj vaše poruke je: [{messageType}] {message}. Također, {newsletterMessage} o daljnjim promjenama preko newslettera.";
 
-            //Kao rezultat se pogled /Views/Home/ContactSuccess.cshtml renderira u "pravi" HTML
-            //Kao parametar se predaje naziv cshtml datoteke koju treba obraditi (ne koristi se default vrijednost)
-            //Trazenu cshtml datoteku je potrebno samostalno dodati u projekt
-            return View(viewName: "ContactSuccess");
+			return RedirectToAction("ContactSuccess");
+		}
+		public IActionResult ContactSuccess()
+		{
+            ViewBag.Message = TempData["Message"];
+
+			return View(viewName: "ContactSuccess");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
