0b088f82f6c8be907168d48c4660b9fda1b166ce
Ivan Cesar <ivan.cesar@splitit.com>
2025-04-25 18:06:34 +0200

    setup vjezba 4

--- Vjezba/Vjezba.Web/Controllers/ClientController.cs
+++ Vjezba/Vjezba.Web/Controllers/ClientController.cs
@@ -9,40 +9,10 @@ namespace Vjezba.Web.Controllers
 	public class ClientController(
         ClientManagerDbContext _dbContext) : Controller
     {
-        public IActionResult Index(string query = null)
+        public IActionResult Index(ClientFilterModel filter = null)
         {
-			var clientQuery = _dbContext.Clients.AsQueryable();
+			filter ??= new ClientFilterModel();
 
-			if (!string.IsNullOrWhiteSpace(query))
-				clientQuery = clientQuery.Where(p => (p.FirstName + " " + p.LastName).ToLower().Contains(query));
-
-			ViewBag.ActiveTab = 1;
-
-            return View(clientQuery.ToList());
-        }
-
-        [HttpPost]
-        public ActionResult Index(string queryName, string queryAddress)
-        {
-			var clientQuery = _dbContext.Clients.AsQueryable();
-
-			//Primjer iterativnog građenja upita - dodaje se "where clause" samo u slučaju da je parametar doista proslijeđen.
-			//To rezultira optimalnijim stablom izraza koje se kvalitetnije potencijalno prevodi u SQL
-			if (!string.IsNullOrWhiteSpace(queryName))
-				clientQuery = clientQuery.Where(p => (p.FirstName + " " + p.LastName).ToLower().Contains(queryName));
-
-			if (!string.IsNullOrWhiteSpace(queryAddress))
-                clientQuery = clientQuery.Where(p => p.Address.ToLower().Contains(queryAddress));
-
-            ViewBag.ActiveTab = 2;
-
-            var model = clientQuery.ToList();
-            return View(model);
-        }
-
-        [HttpPost]
-        public ActionResult AdvancedSearch(ClientFilterModel filter, int tab = 3)
-        {
 			var clientQuery = _dbContext.Clients.AsQueryable();
 
 			//Primjer iterativnog građenja upita - dodaje se "where clause" samo u slučaju da je parametar doista proslijeđen.
@@ -59,10 +29,8 @@ namespace Vjezba.Web.Controllers
             if (!string.IsNullOrWhiteSpace(filter.City))
 				clientQuery = clientQuery.Where(p => p.CityID != null && p.City.Name.ToLower().Contains(filter.City.ToLower()));
 
-			ViewBag.ActiveTab = tab;
-
             var model = clientQuery.ToList();
-            return View("Index", model);
+            return View(model);
         }
 
         public IActionResult Details(int? id = null)
